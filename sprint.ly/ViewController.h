691ba8a6cba28e1f4d8b2b195e323531b6a106e3
Steven W. Riggins <steve@geeksrus.com>
2012-03-04 09:25:15 -0800

    empty project closes #19

---
+++ sprint.ly/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  sprint.ly
+//
+//  Created by Steven Riggins on 3/4/12.
+//  Copyright (c) 2012 Geeks R Us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ sprint.ly/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sprint.ly
+//
+//  Created by Steven Riggins on 3/4/12.
+//  Copyright (c) 2012 Geeks R Us. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
